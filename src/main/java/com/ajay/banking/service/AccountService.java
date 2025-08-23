0cccd9453ef51dae1a5af645711e47acd474a7e1
Ajay S <realajayss@gmail.com>
2025-08-23 19:45:41 +0530

    Service interface

---
+++ src/main/java/com/ajay/banking/service/AccountService.java
@@ -0,0 +1,21 @@
+package com.ajay.banking.service;
+
+import com.ajay.banking.dto.AccountDto;
+
+import java.util.List;
+
+public interface AccountService {
+
+    AccountDto createAccount(AccountDto accountDto);
+
+    AccountDto getAccountById(Long id);
+
+    AccountDto deposit(Long id, double amount);
+
+    AccountDto withdraw(Long id, double amount);
+
+    List<AccountDto> getAllAccounts();
+
+    void deleteAccount(Long id);
+
+}
