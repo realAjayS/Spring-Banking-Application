3b96e45f1039d92fa4ea6d892299f1f5e0f433a4
Ajay S <realajayss@gmail.com>
2025-08-25 08:28:40 +0530

    Code Update

---
+++ src/main/java/com/ajay/banking/dto/AccountDto.java
@@ -0,0 +1,13 @@
+package com.ajay.banking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@AllArgsConstructor
+public class AccountDto {
+    private Long id;
+    private String accountHolderName;
+    private double balance;
+}
